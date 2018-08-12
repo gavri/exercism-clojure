107e16fce0b9a669a2624868c7c32f6690b65a79
Gavri Fernandez <gavri.fernandez@gmail.com>
2018-08-12 11:15:03 +0530

    Armstrong numbers: Make recursive functions tail-recursive

--- armstrong-numbers/src/armstrong_numbers.clj
+++ armstrong-numbers/src/armstrong_numbers.clj
@@ -1,14 +1,18 @@
 (ns armstrong-numbers)
 
 (defn expt [base exponent]
-  (if (= exponent 0) 1
-    (* base (expt base (- exponent 1)))))
+  (defn expt-recur [base exponent result-so-far]
+    (if (= exponent 0) result-so-far
+      (recur base (- exponent 1) (* base result-so-far))))
+  (expt-recur base exponent 1))
 
 (defn digits [num]
-  (cond (= num 0) '()
-        :else (let [r (rem num 10)
-                    q (quot num 10)]
-                (cons r (digits q)))))
+  (defn digits-recur [num result-so-far]
+    (cond (= num 0) result-so-far
+          :else (let [r (rem num 10)
+                      q (quot num 10)]
+                  (recur q (cons r result-so-far)))))
+  (digits-recur num '()))
 
 (defn armstrong? [num]
   (let [ds (digits num)
