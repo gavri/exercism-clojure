9c519031f54ee3b90566c207d7374b90a5a79a67
Gavri Fernandez <gavri.fernandez@gmail.com>
2018-08-14 21:21:35 +0530

    Reverse string: replace recursion with reduce

--- reverse-string/src/reverse_string.clj
+++ reverse-string/src/reverse_string.clj
@@ -1,9 +1,7 @@
 (ns reverse-string)
 
-(defn reverse-sequence [sequence]
-  (loop [sequence sequence result-so-far '()]
-    (cond (= (count sequence) 0) result-so-far
-          :else (recur (rest sequence) (cons (first sequence) result-so-far)))))
+(defn reverse-seq [sequence]
+  (reduce conj '() sequence))
 
 (defn reverse-string [s]
-  (apply str (reverse-sequence s)))
+  (apply str (reverse-seq s)))
