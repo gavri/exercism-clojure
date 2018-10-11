22b1460d6f1ea7f97a8d1a5e20db68f7fb153240
Gavri Fernandez <gavri.fernandez@gmail.com>
2018-10-10 19:33:14 -0700

    Complex numbers: refactored

--- complex-numbers/src/complex_numbers.clj
+++ complex-numbers/src/complex_numbers.clj
@@ -7,25 +7,17 @@
 (defn abs [[a b]]
   (Math/sqrt (+ (* a a) (* b b))))
 
-(defn conjugate [[a b]]
-  [a (- 0 b)]
-  )
+(defn conjugate [[a b]] [a (- b)])
 
 (defn add [[a b] [c d]]
-  [(+ a c) (+ b d)]
-  )
-
+  [(+ a c) (+ b d)])
 
 (defn sub [[a b] [c d]]
-  [(- a c) (- b d)]
-  )
+  [(- a c) (- b d)])
 
 (defn mul [[a b] [c d]]
   [(- (* a c) (* b d)) (+ (* b c) (* a d))])
 
 (defn div [[a b] [c d]]
-  [
-   (double (/ (+ (* a c) (* b d)) (+ (* c c) (* d d))))
-   (double (/ (- (* b c) (* a d)) (+ (* c c) (* d d))))
-   ]
-  )
+  (let [abs-square-of-divisor (+ (* c c) (* d d))]
+    [(double (/ (+ (* a c) (* b d)) abs-square-of-divisor)) (double (/ (- (* b c) (* a d)) abs-square-of-divisor))]))
